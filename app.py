9a903d35cf8f49ab45ff6f03f44c521850ab6004
merycd15 <merycappellanodocampo@live.com>
2025-06-03 21:58:03 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)  # 👈 Habilita CORS para todos los orígenes
+
+DATA_FILE = "data/data.json"
+
+def load_data():
+    with open(DATA_FILE, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    data = load_data()
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
